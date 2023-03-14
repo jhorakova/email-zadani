928cdf4eba4f0ce056bbea944a2ef6f0f153bc6d
Jana Horáková <horakovajana2017@gamil.com>
2023-03-14 22:34:24 +0100

    1

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 console.log('funguju');
+/*Pojďme dále rozvinout cvičení s vyplňováním e-mailu z předchozí lekce.
+
+Opět si naklonujte repozitář se zadáním pro předchozí cvičení.
+Do souboru index.js vložte kód vzorového řešení. Na tomto kódu budeme dále stavět.
+Všimněte si funkce goodbye, která generuje pozdrav na konec e-mailu. Přidejte alespoň dvě další funkce, kde každá generuje k zadanému jménu jiný typ pozdravu. 
+Například formalGoodbye pro velmi formální pozdravy jako „S uctivou poklonou…“, nebo naopak rudeGoodbye typu „Se měj…“, pokud se chcete rozloučit nevybíravě.
+Udělejte z funkce fillBody funkci vyššího řádu. Přidejte jí třetí parametr goodbyeFunction, který představuje funkci, pomocí které se má vygenerovat závěrečný pozdrav. 
+Vyzkoušejte zavolat funkci fillBody postupně s každou z vašich zdravících funkcí a ověřte, že vše správně funguje.
+Ukázka použití
+fillSubject('Obchodní sdělení')
+fillBody(
+	'Kupte mycí prostředek, který vám vytře zrak, se slevou 50 %.',
+	'Jan Čistý',
+	formalGoodbye
+)
+nebo
+
+fillSubject('Pozvánka na oslavu narozenin')
+fillBody('Zítra oslava. 18:00 ve Starý hospodě.', 'Patrik Veselý', rudeGoodbye)*/
+
